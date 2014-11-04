6f9ff954b3889cf0cecd8437cef2915236b6e8ac
Lihan Li <frankieteardrop@gmail.com>
2014-11-04 17:52:43 +0800

    payment receiving model included block

--- lib/bitcoin_payments/models/payment_receiving_model.rb
+++ lib/bitcoin_payments/models/payment_receiving_model.rb
@@ -2,23 +2,17 @@ module BitcoinPayments::Models::PaymentReceivingModel
   extend ActiveSupport::Concern
 
   included do
-    binding.pry; raise
-    # belongs_to(:btc_address, inverse_of: :match_participant_bet, autosave: true, dependent: :destroy)
+    model_name = self.to_s.underscore.to_sym
+
+    belongs_to(:btc_address, inverse_of: model_name, autosave: true, dependent: :destroy)
+    has_many(:received_payments, inverse_of: model_name, autosave: true)
+
+    validates(:btc_address, presence: true)
+
+    before_validation do
+      if btc_address.blank?
+        self.btc_address = BtcAddress.new
+      end
+    end
   end
 end
-
-# belongs_to(:match_participant, inverse_of: :match_participant_bets)
-#   belongs_to(:bet_type, inverse_of: :match_participant_bets)
-#   belongs_to(:btc_address, inverse_of: :match_participant_bet, autosave: true, dependent: :destroy)
-
-#   has_many(:received_payments, inverse_of: :match_participant_bet, autosave: true)
-
-#   validates(:match_participant, :bet_type, :btc_address, presence: true)
-#   validates(:bet_type_id, uniqueness: { scope: :match_participant_id })
-#   validate(:must_have_consistent_bet_type)
-
-#   before_validation do
-#     if btc_address.blank?
-#       self.btc_address = BtcAddress.new
-#     end
-#   end
