5419cc6cf6d99ac20a96189a9c2540f51a849cac
Lihan Li <frankieteardrop@gmail.com>
2015-03-20 14:34:47 -0400

    clean up sent payment

--- lib/bitcoin_payments/models/sent_payment.rb
+++ lib/bitcoin_payments/models/sent_payment.rb
@@ -2,10 +2,9 @@ module BitcoinPayments::Models::SentPayment
   extend ActiveSupport::Concern
 
   included do
-    belongs_to(:payment, inverse_of: :sent_payment, autosave: true)
-    belongs_to(:bet_type, inverse_of: :sent_payments)
+    belongs_to(:payment, inverse_of: :sent_payment)
 
-    validates(:payment, :match, :bet_type, presence: true)
+    validates(:payment, presence: true)
     validates(:payment_id, uniqueness: true)
   end
 end
