5046c3efef75dcf7890fcfab1f2a84d49352147e
Lihan Li <frankieteardrop@gmail.com>
2015-03-20 14:53:53 -0400

    user model not required

--- lib/bitcoin_payments/models/btc_address.rb
+++ lib/bitcoin_payments/models/btc_address.rb
@@ -2,8 +2,6 @@ module BitcoinPayments::Models::BtcAddress
   extend ActiveSupport::Concern
 
   included do
-    belongs_to(:user, inverse_of: :btc_addresses)
-
     has_many(:payments, inverse_of: :btc_address)
 
     validates(:public_key, presence: true, uniqueness: { case_sensitive: true })
