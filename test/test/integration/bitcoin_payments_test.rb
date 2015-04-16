e725a48bc441c39117ca0b997321294955dce4fc
Lihan Li <frankieteardrop@gmail.com>
2015-04-15 21:30:46 -0400

    test all transactions

--- test/test/integration/bitcoin_payments_test.rb
+++ test/test/integration/bitcoin_payments_test.rb
@@ -22,6 +22,7 @@ class BitcoinPaymentsTest < ActiveSupport::TestCase
 
     @client.create_received_payments
     received_payments = ReceivedPayment.all.to_a
+    assert_equal(3, received_payments.size)
 
     [
       {
@@ -29,7 +30,19 @@ class BitcoinPaymentsTest < ActiveSupport::TestCase
         amount: '0.001',
         txid: 'e9c55c74670dd51530989fb39d020a9a39c4b3af75dcc6efc770151b680c8366',
         receiver_key: '1AC8jNajH7zYikxRzRKT3otNW72B9qVDFa',
-      }
+      },
+      {
+        sender_key: '12rARqgZBz1hHeESvknLn2Wt2cJc9ddp1y',
+        amount: '0.001',
+        txid: 'ade4cf44b718b4c338f6962f2501a01dd7e203aa2e2df1bdab6383c1599e0aa6',
+        receiver_key: '1AC8jNajH7zYikxRzRKT3otNW72B9qVDFa',
+      },
+      {
+        sender_key: '1G1ravYqGnpM68CUhp2ckD9PXkvtYit23f',
+        amount: '0.001',
+        txid: '1ae4096365baa4164b8a6f19ff32b7bc0761bde1793e50d9949cec753585c696',
+        receiver_key: '1AC8jNajH7zYikxRzRKT3otNW72B9qVDFa',
+      },
     ].each_with_index do |expected_values, i|
       received_payment = received_payments[i]
       payment = received_payment.payment
