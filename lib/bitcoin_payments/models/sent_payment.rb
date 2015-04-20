92bd70a58cd429537e5060049cb1a63b72632b97
Lihan Li <frankieteardrop@gmail.com>
2015-04-20 15:02:18 -0400

    destroying sent payment will destroy payment

--- lib/bitcoin_payments/models/sent_payment.rb
+++ lib/bitcoin_payments/models/sent_payment.rb
@@ -2,7 +2,7 @@ module BitcoinPayments::Models::SentPayment
   extend ActiveSupport::Concern
 
   included do
-    belongs_to(:payment, inverse_of: :sent_payment)
+    belongs_to(:payment, inverse_of: :sent_payment, dependent: :destroy)
 
     validates(:payment, presence: true)
     validates(:payment_id, uniqueness: true)
