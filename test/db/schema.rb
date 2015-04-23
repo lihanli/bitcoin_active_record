d45c3d1fc9df09d3362dea2ff4b9c12fe60ffce4
Lihan Li <frankieteardrop@gmail.com>
2015-04-23 17:33:44 -0400

    timestamps to all the tables

--- test/db/schema.rb
+++ test/db/schema.rb
@@ -11,33 +11,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416002513) do
+ActiveRecord::Schema.define(version: 20150423213159) do
 
   create_table "btc_addresses", force: true do |t|
-    t.string "public_key", null: false
+    t.string   "public_key", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "btc_addresses", ["public_key"], name: "index_btc_addresses_on_public_key", unique: true
 
   create_table "payments", force: true do |t|
-    t.integer "btc_address_id", null: false
-    t.decimal "amount",         null: false
-    t.string  "txid",           null: false
+    t.integer  "btc_address_id", null: false
+    t.decimal  "amount",         null: false
+    t.string   "txid",           null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "payments", ["btc_address_id"], name: "index_payments_on_btc_address_id"
   add_index "payments", ["txid"], name: "index_payments_on_txid", unique: true
 
   create_table "received_payments", force: true do |t|
-    t.integer "payment_id",     null: false
-    t.integer "btc_address_id", null: false
+    t.integer  "payment_id",     null: false
+    t.integer  "btc_address_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "received_payments", ["btc_address_id"], name: "index_received_payments_on_btc_address_id"
   add_index "received_payments", ["payment_id"], name: "index_received_payments_on_payment_id", unique: true
 
   create_table "sent_payments", force: true do |t|
-    t.integer "payment_id", null: false
+    t.integer  "payment_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "sent_payments", ["payment_id"], name: "index_sent_payments_on_payment_id", unique: true
